80e6877ed7ba7f88c2a6ee7134a7e2ecb9cce236
fkkmemi <fkkmemi@gmail.com>
2023-07-11 16:40:17 +0900

    responsedata export bug fix

--- frame/ResponseData.ts
+++ frame/ResponseData.ts
@@ -1,23 +1,19 @@
-interface ResponseHeader {
-  resultCode: string | number
-  resultMsg: string
+export interface ResponseHeader {
+	resultCode: string | number
+	resultMsg: string
 }
-interface ResponseBody<T> {
-  items:
-    | ''
-    | {
-        item: T | T[]
-      }
-  numOfRows: number
-  pageNo: number
-  totalCount: number
+export interface ResponseBody<T> {
+	items: '' | { item: T | T[] }
+	numOfRows: number
+	pageNo: number
+	totalCount: number
 }
 
-interface ResponseData<T> {
-  response: {
-    body?: ResponseBody<T>
-    header: ResponseHeader
-  }
+export interface ResponseData<T> {
+	response: {
+		body?: ResponseBody<T>
+		header: ResponseHeader
+	}
 }
 
-export default ResponseData
+// export default ResponseData
