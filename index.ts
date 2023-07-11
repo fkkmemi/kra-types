10330f6cdb54007f31121ad382767835cf2242c7
fkkmemi <fkkmemi@gmail.com>
2023-07-11 13:20:01 +0900

    apiURL add

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
-export const BaceURL = 'https://apis.data.go.kr'
+export const baseURL = 'https://apis.data.go.kr'
+export const apiURL = (no: string) =>
+	`https://www.data.go.kr/data/${no}/openapi.do`
 export * from './api/RacePlan'
 export * from './api/RaceResult'
 export * from './api/DailyTraining'
